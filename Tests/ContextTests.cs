5afe9521b40e7b34884ba507890d68b980d7aeef
Robert Wallis <smilingrob@gmail.com>
2017-06-12 12:34:07 -0700

    Tests init.

---
+++ Tests/ContextTests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (C) 2017 Robert A. Wallis, All Rights Reserved.
+
+using NUnit.Framework;
+using SECS;
+
+namespace Tests.SECS
+{
+	[TestFixture]
+	class ContextTests
+	{
+		[Test]
+		public void CreateRemoveEntity()
+		{
+			var context = new Context();
+			var e1 = context.CreateEntity();
+			Assert.NotNull(e1);
+			var e2 = context.CreateEntity();
+			Assert.NotNull(e2);
+
+			context.ReleaseEntity(e1);
+			context.ReleaseEntity(e2);
+		}
+
+		[Test]
+		public void ContextsNotShared()
+		{
+			var c1 = new Context();
+			var e1 = c1.CreateEntity();
+			var c2 = new Context();
+			// TODO: enumerate components
+		}
+
+		[Test]
+		public void Matchers()
+		{
+			var context = new Context();
+			var entity = context.CreateEntity();
+			var matcher = context.Having(typeof(AComponent));
+			Assert.AreEqual(0, matcher.Count);
+			var component = new AComponent("a1");
+			entity.Add(component);
+			var enumerator = matcher.GetEnumerator();
+			enumerator.MoveNext();
+			Assert.AreEqual(component, enumerator.Current);
+		}
+	}
+}
---
+++ Tests/EntityTests.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (C) 2017 Robert A. Wallis, All Rights Reserved.
+
+using NUnit.Framework;
+using SECS;
+
+namespace Tests.SECS
+{
+	[TestFixture]
+	public class EntityTests
+	{
+		[Test]
+		public void Components()
+		{
+			var context = new Context();
+
+			// init
+			var e = context.CreateEntity();
+			Assert.IsFalse(e.Has<AComponent>());
+
+			// add
+			var aComponent = new AComponent("a1");
+			e.Add(aComponent);
+			Assert.IsTrue(e.Has<AComponent>());
+			Assert.AreSame(aComponent, e.Get<AComponent>());
+			var bComponent = new BComponent();
+			Assert.IsFalse(e.Has<BComponent>());
+			e.Add(bComponent);
+			Assert.AreSame(bComponent, e.Get<BComponent>());
+			Assert.AreSame(aComponent, e.Get<AComponent>());
+
+			// replace
+			var aComponent2 = new AComponent("a2");
+			Assert.AreNotSame(aComponent2, e.Get<AComponent>());
+			e.Add(aComponent2);
+			Assert.AreSame(aComponent2, e.Get<AComponent>());
+
+			// remove
+			e.Remove<AComponent>();
+			Assert.IsFalse(e.Has<AComponent>());
+		}
+	}
+
+	class AComponent : IComponent
+	{
+		private readonly string _name;
+
+		public AComponent(string name)
+		{
+			_name = name;
+		}
+
+		public override string ToString()
+		{
+			return _name;
+		}
+	}
+
+	class BComponent : IComponent
+	{
+	}
+}
