f4b879149895ce8ffe57ae987fcc8ed8f75ae9b6
Robert Wallis <smilingrob@gmail.com>
2017-06-16 08:55:59 -0700

    Unity library built with mono 3.5

--- ECSLight Unity/Properties/AssemblyInfo.cs
+++ ECSLight Unity/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ECS Light")]
+[assembly: AssemblyTitle("ECS Light Unity")]
 [assembly: AssemblyDescription("A lightweight ECS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Walnut Cube")]
-[assembly: AssemblyProduct("ECS Light")]
+[assembly: AssemblyProduct("ECSLight Unity")]
 [assembly: AssemblyCopyright("Copyright © Robert A. Wallis 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ff35676-9f33-47fd-9562-9aeb2bb4f786")]
 
 // Version information for an assembly consists of the following four values:
 //
